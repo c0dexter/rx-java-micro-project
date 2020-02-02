4ba304a832988eb6b1f3d6aa542a48e3171d19bd
c0dexter <michal.dobrowolski1@gmail.com>
2020-02-02 23:51:46 +0100

    Created observable by using FROM ARRAY operator

--- app/src/main/java/com/c0dexter/android/rxjava/MainActivity.java
+++ app/src/main/java/com/c0dexter/android/rxjava/MainActivity.java
@@ -21,80 +21,43 @@ public class MainActivity extends AppCompatActivity {
     private TextView text;
     private String TAG = getClass().getName();
 
-    // vars
-    private CompositeDisposable disposables = new CompositeDisposable(); // Create this object (it's a list) for CLEANING OBSERVERS
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
+        Task[] list = new Task[5];
+        list[0] = (new Task("Take out the trash", true, 3));
+        list[1] = (new Task("Walk the dog", false, 2));
+        list[2] = (new Task("Make my bed", true, 1));
+        list[3] = (new Task("Unload the dishwasher", false, 0));
+        list[4] = (new Task("Make dinner", true, 5));
 
-//        This is a very basic example.
-//        But it showcases the general structure of every Observable / Observer interaction.
-//
-//        1. Create an Observable
-//        2. Apply an operator to the Observable
-//        3. Designate what thread to do the work on and what thread to emit the results to
-//        4. Subscribe an Observer to the Observable and view the results
+        Observable<Task> taskObservable = Observable
+                .fromArray(list)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
 
-        Observable<Task> taskObservable = Observable        // Create observable
-                .fromIterable(DataSource.createTasksList()) // Apply operator to the Observable
-                .subscribeOn(Schedulers.io())               // Subscribe on a tread
-                .filter(new Predicate<Task>() {
-                    @Override
-                    public boolean test(Task task) throws Exception {              // (another operator if we want)
-                        Log.d(TAG, "Test: " + Thread.currentThread().getName());
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return task.isComplete();
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread()); // Describe where you observe on
-
-
-        taskObservable.subscribe(new Observer<Task>() {     // Subscribe observers which will view the results on a the designated thread as observe thread
+        taskObservable.subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-                disposables.add(d);     // Add this disposable (d) to the disposables list
-            }
 
+            }
 
             @Override
             public void onNext(Task task) {
-                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
+                Log.d(TAG, "onNext: : " + task.getDescription());
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, "onError: " + e);
+
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete: called");
+
             }
         });
-
-        // There is more possibilities to create observers and subscribe to observables
-        disposables.add(taskObservable.subscribe(new Consumer<Task>() { // this implementation returns disposables, so we can add it to disposables here
-            @Override
-            public void accept(Task task) throws Exception {
-
-            }
-        }));
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disposables.clear();    // .clear is using for clearing observers without clearing observables
-        // .dispose is using for HARD CLEARING observers with observables
-        // In MVVM architecture, clearing disposables is executed in ViewModel class in the onClear() method
     }
 }
