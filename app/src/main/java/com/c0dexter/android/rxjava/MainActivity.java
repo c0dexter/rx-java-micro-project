d14b355a4ae2637d96f17ff48ac798ccee8e05ab
c0dexter <michal.dobrowolski1@gmail.com>
2020-02-02 23:14:36 +0100

    Created a time counter by using INTERVAL operator

--- app/src/main/java/com/c0dexter/android/rxjava/MainActivity.java
+++ app/src/main/java/com/c0dexter/android/rxjava/MainActivity.java
@@ -6,12 +6,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
@@ -30,71 +31,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
 
-//        This is a very basic example.
-//        But it showcases the general structure of every Observable / Observer interaction.
-//
-//        1. Create an Observable
-//        2. Apply an operator to the Observable
-//        3. Designate what thread to do the work on and what thread to emit the results to
-//        4. Subscribe an Observer to the Observable and view the results
-
-        Observable<Task> taskObservable = Observable        // Create observable
-                .fromIterable(DataSource.createTasksList()) // Apply operator to the Observable
-                .subscribeOn(Schedulers.io())               // Subscribe on a tread
-                .filter(new Predicate<Task>() {
+        // emit an observable every time interval
+        Observable<Long> intervalObservable = Observable
+                .interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .takeWhile(new Predicate<Long>() { // stop the process if more than 5 seconds passes
                     @Override
-                    public boolean test(Task task) throws Exception {              // (another operator if we want)
-                        Log.d(TAG, "Test: " + Thread.currentThread().getName());
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return task.isComplete();
+                    public boolean test(Long aLong) throws Exception {
+                        Log.d(TAG, "test: " + aLong + ", on thread: " + Thread.currentThread().getName());
+                        return aLong <= 5;
                     }
                 })
-                .observeOn(AndroidSchedulers.mainThread()); // Describe where you observe on
+                .observeOn(AndroidSchedulers.mainThread());
 
-
-        taskObservable.subscribe(new Observer<Task>() {     // Subscribe observers which will view the results on a the designated thread as observe thread
+        intervalObservable.subscribe(new Observer<Long>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-                disposables.add(d);     // Add this disposable (d) to the disposables list
+
             }
 
-
             @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
+            public void onNext(Long aLong) {
+                Log.d(TAG, "onNext: " + aLong);
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, "onError: " + e);
+
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete: called");
+
             }
         });
-
-        // There is more possibilities to create observers and subscribe to observables
-        disposables.add(taskObservable.subscribe(new Consumer<Task>() { // this implementation returns disposables, so we can add it to disposables here
-            @Override
-            public void accept(Task task) throws Exception {
-
-            }
-        }));
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disposables.clear();    // .clear is using for clearing observers without clearing observables
-        // .dispose is using for HARD CLEARING observers with observables
-        // In MVVM architecture, clearing disposables is executed in ViewModel class in the onClear() method
     }
 }
